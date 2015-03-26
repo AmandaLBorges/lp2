6327f3d83fae9ec8a4532294bdaa778e7f7515d0
AmandaLBorges <amandalemesbor@gmail.com>
2015-03-26 14:47:07 -0300

    Update Exponencial.java
    EXPONENCIAL

--- repositorio/Exponencial.java
+++ repositorio/Exponencial.java
@@ -14,3 +14,14 @@ System.out.printf("%d\t%d\t%d%n", i, i*i, i*i*i);
   }
 
 }
+
+
+public class Exponencial {
+
+    public static void main(String[] args) {
+        System.out.println("# Q C");
+        for(int i=0; i<=10; i++){
+            System.out.println(i + " " + (i*i) + " " + (i*i*i));
+        }
+    }
+}
